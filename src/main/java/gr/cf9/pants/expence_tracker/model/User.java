260d8998c1d9682ef6c57ceabdbd0863a7210997
PanTs <pants.ath@gmail.com>
2026-02-21 16:31:15 +0200

    feat: add User entity

---
+++ src/main/java/gr/cf9/pants/expence_tracker/model/User.java
@@ -0,0 +1,49 @@
+package gr.cf9.pants.expence_tracker.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+/**
+ * Represents a registered user in the application.
+ * <p>
+ * This entity maps to the "users" table and serves as the core identity for the system.
+ * It stores login credentials and links the user to their financial data
+ * (accounts, transactions, etc.).
+ * </p>
+ *
+ * @author PanTs
+ */
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User extends AbstractEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, updatable = false)
+    private UUID uuid;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private boolean isActive;
+
+}
