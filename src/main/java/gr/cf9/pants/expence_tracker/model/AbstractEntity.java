94a9766cc4a933469e203325f5fe560e6577a314
PanTs <pants.ath@gmail.com>
2026-02-21 15:56:55 +0200

    feat: add abstract entity to inherit and use createdAt and updatedAt

---
+++ src/main/java/gr/cf9/pants/expence_tracker/model/AbstractEntity.java
@@ -0,0 +1,32 @@
+package gr.cf9.pants.expence_tracker.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@MappedSuperclass       // no db table, just inheritance
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)      // +JpaAuditing -> automate auditing for fields: CreatedDate and LastModifiedDate
+public abstract class AbstractEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "DATETIME")
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME")
+    private Instant updatedAt;
+
+}
