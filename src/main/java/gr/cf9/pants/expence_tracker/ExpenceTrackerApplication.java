8c8933ae4f18f3527b3ffad928a3cb1013bf9295
PanTs <pants.ath@gmail.com>
2026-02-21 15:32:38 +0200

    feat: add jpa auditing for keeping record (timestamp)

---
+++ src/main/java/gr/cf9/pants/expence_tracker/ExpenceTrackerApplication.java
@@ -0,0 +1,15 @@
+package gr.cf9.pants.expence_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing		//timestamp
+@SpringBootApplication
+public class ExpenceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenceTrackerApplication.class, args);
+	}
+
+}
