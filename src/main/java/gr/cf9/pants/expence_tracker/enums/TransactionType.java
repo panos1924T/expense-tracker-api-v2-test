75b32f7120a32f8188248a1965aa6ab9a72faac3
PanTs <pants.ath@gmail.com>
2026-02-21 16:56:02 +0200

    feat: add enum -> type of transaction

---
+++ src/main/java/gr/cf9/pants/expence_tracker/enums/TransactionType.java
@@ -0,0 +1,8 @@
+package gr.cf9.pants.expence_tracker.enums;
+
+public enum TransactionType {
+
+    INCOME,     // Income (Salary -> Account)
+    EXPENSE,    // Expense (Account -> Supermarket)
+    TRANSFER    // Transfer (Account A -> Account B)
+}
