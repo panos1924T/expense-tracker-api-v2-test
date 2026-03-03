698b21aaeeb919bf88d8e1c2b0d7b2148f548a4a
PanTs <pants.ath@gmail.com>
2026-03-03 15:57:23 +0200

    feat: add optional find by uuid

--- src/main/java/gr/cf9/pants/expence_tracker/repository/UserRepository.java
+++ src/main/java/gr/cf9/pants/expence_tracker/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -15,5 +16,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
 
-
+    Optional<User> findBuUuid(UUID uuid);
 }
