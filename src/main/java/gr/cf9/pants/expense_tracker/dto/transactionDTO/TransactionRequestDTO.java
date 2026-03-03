d977dc12bc02f3aff9f8a76a1318e57a2e3702e1
PanTs <pants.ath@gmail.com>
2026-03-03 17:27:42 +0200

    feat: initialize transaction request dto

---
+++ src/main/java/gr/cf9/pants/expense_tracker/dto/transactionDTO/TransactionRequestDTO.java
@@ -0,0 +1,15 @@
+package gr.cf9.pants.expense_tracker.dto.transactionDTO;
+
+import gr.cf9.pants.expense_tracker.enums.TransactionType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record TransactionRequestDTO(
+   Long sourceAccountId,
+   Long categoryId,
+   TransactionType type,
+   BigDecimal amount,
+   LocalDate transactionDate,
+   String description
+) {}
