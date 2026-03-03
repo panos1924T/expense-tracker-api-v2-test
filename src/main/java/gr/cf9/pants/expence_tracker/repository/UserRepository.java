1177df28b359f30c354e75b6a2bbeab0a175d0e3
PanTs <pants.ath@gmail.com>
2026-03-03 15:38:01 +0200

    feat: initialize user repository

---
+++ src/main/java/gr/cf9/pants/expence_tracker/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package gr.cf9.pants.expence_tracker.repository;
+
+import gr.cf9.pants.expence_tracker.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+
+
+}
