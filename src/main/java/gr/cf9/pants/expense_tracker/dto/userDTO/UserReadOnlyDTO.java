57caaa6f73a8a288fb5f1e22f9036344ad67f071
PanTs <pants.ath@gmail.com>
2026-03-03 17:21:35 +0200

    fix: string => instant createdAt

--- src/main/java/gr/cf9/pants/expense_tracker/dto/userDTO/UserReadOnlyDTO.java
+++ src/main/java/gr/cf9/pants/expense_tracker/dto/userDTO/UserReadOnlyDTO.java
@@ -1,10 +1,11 @@
 package gr.cf9.pants.expense_tracker.dto.userDTO;
 
+import java.time.Instant;
 import java.util.UUID;
 
 public record UserReadOnlyDTO(
         UUID uuid,
         String username,
         String email,
-        String createdAt
+        Instant createdAt
 ) {}
