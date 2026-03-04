c2456ca2a0b84cdc88a6f9aa82690a9ab9d491e1
PanTs <pants.ath@gmail.com>
2026-03-04 17:31:18 +0200

    feat: initialize category mapper

---
+++ src/main/java/gr/cf9/pants/expense_tracker/mapper/CategoryMapper.java
@@ -0,0 +1,31 @@
+package gr.cf9.pants.expense_tracker.mapper;
+
+import gr.cf9.pants.expense_tracker.dto.category_dto.CategoryCreateDTO;
+import gr.cf9.pants.expense_tracker.dto.category_dto.CategoryReadOnlyDTO;
+import gr.cf9.pants.expense_tracker.model.Category;
+import gr.cf9.pants.expense_tracker.model.User;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class CategoryMapper {
+
+    public CategoryReadOnlyDTO toReadOnly(Category category) {
+        return new CategoryReadOnlyDTO(
+                category.getId(),
+                category.getName(),
+                category.getType(),
+                category.getParent() != null ? category.getParent().getId() : null,
+                category.getParent() != null ? category.getParent().getName() : null,
+                category.isSystemDefault()
+        );
+    }
+
+    public Category toEntity(CategoryCreateDTO dto, User user) {
+        Category category = new Category();
+        category.setUser(user);
+        category.setName(dto.name());
+        category.setType(dto.type());
+        return category;
+    }
+}
