f2862206883dc8ffbc9a682af8190b15f45d44d8
PanTs <pants.ath@gmail.com>
2026-03-05 16:55:32 +0200

    feat: initialize transaction update dto

---
+++ src/main/java/gr/cf9/pants/expense_tracker/dto/transaction_dto/TransactionUpdateDTO.java
@@ -0,0 +1,11 @@
+package gr.cf9.pants.expense_tracker.dto.transaction_dto;
+
+import gr.cf9.pants.expense_tracker.model.Category;
+
+import java.time.LocalDate;
+
+public record TransactionUpdateDTO(
+        String description,
+        LocalDate transactionDate,
+        Category category
+) {}
