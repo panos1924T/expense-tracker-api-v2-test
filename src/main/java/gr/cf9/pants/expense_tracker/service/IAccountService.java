1037ad6faf0149761f69d72de02c87245a66c0c1
PanTs <pants.ath@gmail.com>
2026-03-05 16:49:55 +0200

    feat: initialize account service interface

---
+++ src/main/java/gr/cf9/pants/expense_tracker/service/IAccountService.java
@@ -0,0 +1,19 @@
+package gr.cf9.pants.expense_tracker.service;
+
+import gr.cf9.pants.expense_tracker.dto.account_dto.AccountCreateDTO;
+import gr.cf9.pants.expense_tracker.dto.account_dto.AccountReadOnlyDTO;
+import gr.cf9.pants.expense_tracker.model.User;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface IAccountService {
+
+    AccountReadOnlyDTO createAccount(AccountCreateDTO dto, UUID userUuid);
+
+    List<AccountReadOnlyDTO> getAllAccounts(UUID userUuid);
+
+    void deleteAccount(Long id, UUID userUuid);
+
+    AccountReadOnlyDTO getAccount(Long id, UUID userUuid);
+}
