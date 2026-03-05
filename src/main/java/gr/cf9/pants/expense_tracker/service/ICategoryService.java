aade03b0d6b9d75630e2009af2725d8a9dd0a574
PanTs <pants.ath@gmail.com>
2026-03-05 18:22:51 +0200

    feat: add id parameter to update method and add 3 more methods getSomething

--- src/main/java/gr/cf9/pants/expense_tracker/service/ICategoryService.java
+++ src/main/java/gr/cf9/pants/expense_tracker/service/ICategoryService.java
@@ -1,18 +1,24 @@
 package gr.cf9.pants.expense_tracker.service;
 
+import gr.cf9.pants.expense_tracker.core.enums.TransactionType;
 import gr.cf9.pants.expense_tracker.dto.category_dto.CategoryCreateDTO;
 import gr.cf9.pants.expense_tracker.dto.category_dto.CategoryReadOnlyDTO;
 import gr.cf9.pants.expense_tracker.dto.category_dto.CategoryUpdateDTO;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ICategoryService {
 
     CategoryReadOnlyDTO createCategory(CategoryCreateDTO dto, UUID userUuid);
 
-    CategoryReadOnlyDTO updateCategory(CategoryUpdateDTO dto, UUID userUuid);
+    CategoryReadOnlyDTO updateCategory(Long id, CategoryUpdateDTO dto, UUID userUuid);
 
     void deleteCategory(Long id, UUID userUuid);
 
+    CategoryReadOnlyDTO getCategoryById(Long id, UUID userUuid);
 
+    List<CategoryReadOnlyDTO> getAllCategories(UUID userUuid);
+
+    List<CategoryReadOnlyDTO> getCategoryByType(TransactionType type, UUID userUuid);
 }
