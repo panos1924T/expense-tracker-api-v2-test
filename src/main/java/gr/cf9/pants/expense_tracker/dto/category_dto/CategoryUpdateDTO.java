0844715bb0b90763a6fc77075fdf6cd950da4552
PanTs <pants.ath@gmail.com>
2026-03-05 16:57:58 +0200

    feat: initialize category update dto

---
+++ src/main/java/gr/cf9/pants/expense_tracker/dto/category_dto/CategoryUpdateDTO.java
@@ -0,0 +1,8 @@
+package gr.cf9.pants.expense_tracker.dto.category_dto;
+
+import gr.cf9.pants.expense_tracker.model.Category;
+
+public record CategoryUpdateDTO(
+        String name,
+        Category parent
+) {}
