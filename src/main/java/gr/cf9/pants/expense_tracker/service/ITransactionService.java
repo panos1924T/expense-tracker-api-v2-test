924190e37a776add9bec055bb9b12ccb46eaadeb
PanTs <pants.ath@gmail.com>
2026-03-05 18:58:31 +0200

    feat: initialize transaction interface for service

---
+++ src/main/java/gr/cf9/pants/expense_tracker/service/ITransactionService.java
@@ -0,0 +1,29 @@
+package gr.cf9.pants.expense_tracker.service;
+
+import gr.cf9.pants.expense_tracker.core.enums.TransactionType;
+import gr.cf9.pants.expense_tracker.dto.transaction_dto.TransactionReadOnlyDTO;
+import gr.cf9.pants.expense_tracker.dto.transaction_dto.TransactionUpdateDTO;
+import gr.cf9.pants.expense_tracker.dto.transaction_dto.TransferCreateDTO;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ITransactionService {
+
+        TransactionReadOnlyDTO createTransaction(TransferCreateDTO dto, UUID userUuid);
+
+        TransactionReadOnlyDTO createTransfer(TransferCreateDTO dto, UUID userUuid);
+
+        TransactionReadOnlyDTO getTransaction(Long id, UUID userUuid);
+
+        TransactionReadOnlyDTO updateTransaction(Long id, TransactionUpdateDTO dto, UUID userUuid);
+
+        List<TransactionReadOnlyDTO> getAllTransactions(UUID userUuid, Pageable pageable);
+
+        List<TransactionReadOnlyDTO> getTransactionByAccount(Long accountId, UUID userUuid, Pageable pageable);
+
+        List<TransactionReadOnlyDTO> getTransactionByType(TransactionType type, UUID userUuid, Pageable pageable);
+
+        void deleteTransaction(Long id, UUID userUuid);
+}
