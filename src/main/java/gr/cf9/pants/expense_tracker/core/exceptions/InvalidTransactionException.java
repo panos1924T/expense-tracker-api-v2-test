fea8646e9b19b208571e1549ffb5caed22fdde49
PanTs <pants.ath@gmail.com>
2026-03-06 17:14:15 +0200

    chore: rename final variable

--- src/main/java/gr/cf9/pants/expense_tracker/core/exceptions/InvalidTransactionException.java
+++ src/main/java/gr/cf9/pants/expense_tracker/core/exceptions/InvalidTransactionException.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class InvalidTransactionException extends Exception {
-    private final String Code = "INVALID_TRANSACTION";
+    private final String code = "INVALID_TRANSACTION";
 
     public InvalidTransactionException(String message) {
         super(message);
