1ea4b85a69f82a67b0ba93b807e16fd838d86a14
PanTs <pants.ath@gmail.com>
2026-03-06 17:12:15 +0200

    feat: add final code to checked exceptions

--- src/main/java/gr/cf9/pants/expense_tracker/core/exceptions/InsufficientBalanceException.java
+++ src/main/java/gr/cf9/pants/expense_tracker/core/exceptions/InsufficientBalanceException.java
@@ -1,6 +1,11 @@
 package gr.cf9.pants.expense_tracker.core.exceptions;
 
+import lombok.Getter;
+
+@Getter
 public class InsufficientBalanceException extends Exception {
+    private final String code = "INSUFFICIENT_BALANCE";
+
     public InsufficientBalanceException(String message) {
         super(message);
     }
--- src/main/java/gr/cf9/pants/expense_tracker/core/exceptions/InvalidTransactionException.java
+++ src/main/java/gr/cf9/pants/expense_tracker/core/exceptions/InvalidTransactionException.java
@@ -1,6 +1,11 @@
 package gr.cf9.pants.expense_tracker.core.exceptions;
 
+import lombok.Getter;
+
+@Getter
 public class InvalidTransactionException extends Exception {
+    private final String Code = "INVALID_TRANSACTION";
+
     public InvalidTransactionException(String message) {
         super(message);
     }
