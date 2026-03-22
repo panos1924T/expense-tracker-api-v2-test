336a92879d86b66db8b1f02cd3c21196ac91159b
pantsath <pants.ath@gmail.com>
2026-03-22 15:42:25 +0200

    feat: initialize category service with annotations and dependencies

---
+++ src/main/java/gr/cf9/pants/expense_tracker/service/CategoryService.java
@@ -0,0 +1,122 @@
+package gr.cf9.pants.expense_tracker.service;
+
+import gr.cf9.pants.expense_tracker.core.enums.TransactionType;
+import gr.cf9.pants.expense_tracker.core.exceptions.EntityNotFoundException;
+import gr.cf9.pants.expense_tracker.dto.category_dto.CategoryCreateDTO;
+import gr.cf9.pants.expense_tracker.dto.category_dto.CategoryReadOnlyDTO;
+import gr.cf9.pants.expense_tracker.dto.category_dto.CategoryUpdateDTO;
+import gr.cf9.pants.expense_tracker.mapper.CategoryMapper;
+import gr.cf9.pants.expense_tracker.model.Category;
+import gr.cf9.pants.expense_tracker.model.User;
+import gr.cf9.pants.expense_tracker.repository.CategoryRepository;
+import gr.cf9.pants.expense_tracker.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CategoryService implements ICategoryService{
+
+    private final CategoryMapper categoryMapper;
+    private final UserRepository userRepository;
+    private final CategoryRepository categoryRepository;
+
+    @Transactional
+    @Override
+    public CategoryReadOnlyDTO createCategory(CategoryCreateDTO dto, UUID userUuid) {
+        //VALIDATE
+        User user = userRepository.findByUuid(userUuid)
+                .orElseThrow(() -> new EntityNotFoundException("User with uuid: " + userUuid + "does not exist!"));
+
+        //PREPARE
+
+
+        //EXECUTE
+
+
+        //RETURN
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public CategoryReadOnlyDTO updateCategory(Long id, CategoryUpdateDTO dto, UUID userUuid) {
+        //VALIDATE
+
+
+        //PREPARE
+
+
+        //EXECUTE
+
+
+        //RETURN
+        return null;
+    }
+
+    @Transactional
+    @Override
+    public void deleteCategory(Long id, UUID userUuid) {
+        //VALIDATE
+
+
+        //PREPARE
+
+
+        //EXECUTE
+
+
+        //RETURN
+    }
+
+    @Override
+    public CategoryReadOnlyDTO getCategoryById(Long id, UUID userUuid) {
+        //VALIDATE
+
+
+        //PREPARE
+
+
+        //EXECUTE
+
+
+        //RETURN
+        return null;
+    }
+
+    @Override
+    public List<CategoryReadOnlyDTO> getAllCategories(UUID userUuid) {
+        //VALIDATE
+
+
+        //PREPARE
+
+
+        //EXECUTE
+
+
+        //RETURN
+        return List.of();
+    }
+
+    @Override
+    public List<CategoryReadOnlyDTO> getCategoryByType(TransactionType type, UUID userUuid) {
+        //VALIDATE
+
+
+        //PREPARE
+
+
+        //EXECUTE
+
+
+        //RETURN
+        return List.of();
+    }
+}
