d1cadeb633d90d5c148eeb4f2424f9f1a34811ec
PanTs <pants.ath@gmail.com>
2026-03-23 19:20:35 +0200

    chore: remove unused import

--- src/main/java/gr/cf9/pants/expense_tracker/service/UserService.java
+++ src/main/java/gr/cf9/pants/expense_tracker/service/UserService.java
@@ -14,7 +14,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.beans.Encoder;
 import java.time.Instant;
 import java.util.UUID;
 
