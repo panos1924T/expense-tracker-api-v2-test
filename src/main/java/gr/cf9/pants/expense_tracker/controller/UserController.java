685553ccc18cf0053c0cbf6dfbd7f79d27ccf5f8
pantsath <pants.ath@gmail.com>
2026-03-25 11:01:53 +0200

    feat: add register post mapping annotation

--- src/main/java/gr/cf9/pants/expense_tracker/controller/UserController.java
+++ src/main/java/gr/cf9/pants/expense_tracker/controller/UserController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PostMapping("/register")
     public ResponseEntity<UserReadOnlyDTO> register(
             @Valid @RequestBody UserRegisterDTO dto) {
 
