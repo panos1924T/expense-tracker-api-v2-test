0d6c36a7db4e64a6ecd591e40b86fa2c5cd721ec
pantsath <pants.ath@gmail.com>
2026-03-25 10:59:52 +0200

    feat: initialize register response entity

--- src/main/java/gr/cf9/pants/expense_tracker/controller/UserController.java
+++ src/main/java/gr/cf9/pants/expense_tracker/controller/UserController.java
@@ -1,7 +1,13 @@
 package gr.cf9.pants.expense_tracker.controller;
 
+import gr.cf9.pants.expense_tracker.dto.user_dto.UserReadOnlyDTO;
+import gr.cf9.pants.expense_tracker.dto.user_dto.UserRegisterDTO;
 import gr.cf9.pants.expense_tracker.service.IUserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +22,11 @@ public class UserController {
         this.userService = userService;
     }
 
+    public ResponseEntity<UserReadOnlyDTO> register(
+            @Valid @RequestBody UserRegisterDTO dto) {
+
+        UserReadOnlyDTO responseDTO = userService.register(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
+    }
 
 }
