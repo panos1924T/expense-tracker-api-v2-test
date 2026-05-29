0dc7cf0b3e05bfa8cd3179ecfe78ea04d4260f7a
PanTs <pants.ath@gmail.com>
2026-05-29 18:06:30 +0300

    feat: implement custom access denied handler for improved error responses

---
+++ src/main/java/gr/cf9/pants/expense_tracker/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,36 @@
+package gr.cf9.pants.expense_tracker.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gr.cf9.pants.expense_tracker.dto.ErrorResponseDTO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException)
+            throws IOException, ServletException {
+        log.warn("Access denied for user to request={} with message={}", request.getRequestURI(), accessDeniedException.getMessage());
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json; charset=UTF-8");
+
+        response.getWriter().write(
+                objectMapper.writeValueAsString(
+                        new ErrorResponseDTO("ACCESS_DENIED", "User is not allowed to access this route.")
+                )
+        );
+    }
+}
