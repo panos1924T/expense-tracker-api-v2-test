2ac923a355b6fe29606c9a5465547b6ba1d2ef21
PanTs <pants.ath@gmail.com>
2026-06-07 19:57:29 +0300

    feat: add constructor to ErrorResponseDTO for simplified initialization with code only

--- src/main/java/gr/cf9/pants/expense_tracker/dto/ErrorResponseDTO.java
+++ src/main/java/gr/cf9/pants/expense_tracker/dto/ErrorResponseDTO.java
@@ -3,4 +3,8 @@ package gr.cf9.pants.expense_tracker.dto;
 public record ErrorResponseDTO(
         String code,
         String message
-) {}
+) {
+    public ErrorResponseDTO(String code) {
+        this(code, "");
+    }
+}
