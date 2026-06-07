101434dbc84811bf90e44acb7a624e2adbebc124
PanTs <pants.ath@gmail.com>
2026-06-07 20:30:44 +0300

    feat: enhance error handling with additional exception handlers and logging in ErrorHandler

--- src/main/java/gr/cf9/pants/expense_tracker/core/filters/ErrorHandler.java
+++ src/main/java/gr/cf9/pants/expense_tracker/core/filters/ErrorHandler.java
@@ -2,16 +2,51 @@ package gr.cf9.pants.expense_tracker.core.filters;
 
 import gr.cf9.pants.expense_tracker.core.exceptions.*;
 import gr.cf9.pants.expense_tracker.dto.ErrorResponseDTO;
+import gr.cf9.pants.expense_tracker.dto.ValidationErrorResponseDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
+@Slf4j
 public class ErrorHandler {
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ValidationErrorResponseDTO> handleValidation(ValidationException e) {
+        log.warn("Validation failed. Message={}", e.getMessage());
+
+        BindingResult bindingResult = e.getBindingResult();
+
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ValidationErrorResponseDTO(
+                        e.getCode(),
+                        e.getMessage(),
+                        errors
+                ));
+    }
 
     @ExceptionHandler(EntityAlreadyExistsException.class)
     public ResponseEntity<ErrorResponseDTO> handleAlreadyExists(EntityAlreadyExistsException e) {
+        log.warn("Entity already exists. Message={}", e.getMessage());
+
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
                 .body(new ErrorResponseDTO(
@@ -22,6 +57,8 @@ public class ErrorHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorResponseDTO> handleNotFound(EntityNotFoundException e) {
+        log.warn("Entity not found. Message={}", e.getMessage());
+
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponseDTO(
@@ -30,18 +67,10 @@ public class ErrorHandler {
                 ));
     }
 
-    @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<ErrorResponseDTO> handleUnauthorized(UnauthorizedException e) {
-        return ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED)
-                .body(new ErrorResponseDTO(
-                        e.getCode(),
-                        e.getMessage()
-                ));
-    }
+    @ExceptionHandler(InvalidArgumentException.class)
+    public ResponseEntity<ErrorResponseDTO> handleInvalidArgument(InvalidArgumentException e) {
+        log.warn("Invalid argument. Message={}", e.getMessage());
 
-    @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<ErrorResponseDTO> handleValidation(ValidationException e) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ErrorResponseDTO(
@@ -52,11 +81,76 @@ public class ErrorHandler {
 
     @ExceptionHandler(InvalidTransactionException.class)
     public ResponseEntity<ErrorResponseDTO> handleInvalidTransaction(InvalidTransactionException e) {
+        log.warn("Invalid transaction. Message={}", e.getMessage());
+
         return ResponseEntity
                 .status(HttpStatus.UNPROCESSABLE_ENTITY)
                 .body(new ErrorResponseDTO(
                         e.getCode(),
                         e.getMessage()
                 ));
+    }
+
+    @ExceptionHandler(EntityHasTransactionsException.class)
+    public ResponseEntity<ErrorResponseDTO> handleHasTransactions(EntityHasTransactionsException e) {
+        log.warn("Entity has transactions. Message={}", e.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ErrorResponseDTO(
+                        e.getCode(),
+                        e.getMessage()
+                ));
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<ErrorResponseDTO> handleDatabaseError(DataAccessException e) {
+        log.warn("Database error. Message={}", e.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponseDTO(
+                        "DATABASE_ERROR",
+                        "A database error occurred"
+                ));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleUnexpectedError(Exception e) {
+        log.warn("Unexpected error. Message={}", e.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponseDTO(
+                        "INTERNAL_SERVER_ERROR",
+                        "Unexpected error occurred"
+                ));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponseDTO> handleAuthentication(AuthenticationException e,
+                                                                 HttpServletRequest request) {
+        log.warn("Failed login for IP={}", request.getRemoteAddr());
+
+        String errorCode = switch (e) {
+            case BadCredentialsException ex -> "INVALID_CREDENTIALS";
+            case DisabledException ex -> "ACCOUNT_DISABLED";
+            case LockedException ex -> "ACCOUNT_LOCKED";
+            case AccountExpiredException ex -> "ACCOUNT_EXPIRED";
+            case CredentialsExpiredException ex -> "CREDENTIALS_EXPIRED";
+            default -> "AUTHENTICATION_ERROR";
+        };
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponseDTO(errorCode, e.getMessage()));
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponseDTO> handleAccessDeniedException(AccessDeniedException e) {
+        log.warn("Access denied. Message={}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)       // 403 Forbidden
+                .body(new ErrorResponseDTO("ACCESS_DENIED", e.getMessage()));
     }
 }
