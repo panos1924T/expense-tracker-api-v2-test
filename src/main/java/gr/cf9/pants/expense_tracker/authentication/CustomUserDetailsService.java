71efc919edc982aa5855ecd0a460d934ad3bc065
PanTs <pants.ath@gmail.com>
2026-06-10 20:35:59 +0300

    feat: implement CustomUserDetailsService for user authentication

---
+++ src/main/java/gr/cf9/pants/expense_tracker/authentication/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package gr.cf9.pants.expense_tracker.authentication;
+
+import gr.cf9.pants.expense_tracker.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email=" + email + " not found"));
+    }
+}
