df6b08767a346b4e48d51744f422291c8ad2f2de
PanTs <pants.ath@gmail.com>
2026-06-10 17:44:10 +0300

    feat: add validation annotations in UserInsertDTO

--- src/main/java/gr/cf9/pants/expense_tracker/dto/user_dto/UserInsertDTO.java
+++ src/main/java/gr/cf9/pants/expense_tracker/dto/user_dto/UserInsertDTO.java
@@ -1,6 +1,18 @@
 package gr.cf9.pants.expense_tracker.dto.user_dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public record UserInsertDTO(
+
+        @Email
+        @NotNull
         String email,
+
+        @NotNull
         String username,
+
+        @NotNull
+        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=])^.{8,}$")
         String password) {}
